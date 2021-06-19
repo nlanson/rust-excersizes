02dfc59373e713a9aad3c6bfdf5eacacc15e9ecf
Noah Lanson <lanson.noah@gmail.com>
2021-06-19 22:38:36 +1000

    move identicon program to own repo

---
+++ modules/target/rls/debug/deps/modules-989996c223bb2c41.d
@@ -0,0 +1,5 @@
+c:\Users\Nlanson\Desktop\Coding\rust-excersizes\modules\target\rls\debug\deps\modules-989996c223bb2c41.rmeta: src\main.rs
+
+c:\Users\Nlanson\Desktop\Coding\rust-excersizes\modules\target\rls\debug\deps\modules-989996c223bb2c41.d: src\main.rs
+
+src\main.rs:
---
+++ modules/target/rls/debug/deps/modules-ac0f40dc87f28289.d
@@ -0,0 +1,5 @@
+c:\Users\Nlanson\Desktop\Coding\rust-excersizes\modules\target\rls\debug\deps\modules-ac0f40dc87f28289.rmeta: src\main.rs
+
+c:\Users\Nlanson\Desktop\Coding\rust-excersizes\modules\target\rls\debug\deps\modules-ac0f40dc87f28289.d: src\main.rs
+
+src\main.rs:
